2072df0292b8a6468a24ed5ab5e512020250d537
Joey Gibson <joey@joeygibson.com>
2014-06-08 22:44:59 -0400

    Updated first example for style

--- src/lolclojure/guess.clj
+++ src/lolclojure/guess.clj
@@ -1,21 +1,31 @@
 (ns lolclojure.guess)
 
-(def *small* (ref 1))
-(def *big* (ref 100))
+;; Using refs for these is overkill, but the original
+;; used mutable global variables.
+(def small (ref 1))
+(def big (ref 100))
 
-(defn guess-my-number []
-  (Math/round (Math/floor (double (/ (+ @*small* @*big*) 2)))))
+(defn guess-my-number
+  "This is, effectively, a binary search between the two extremes."
+  []
+  (Math/round (Math/floor (double (/ (+ @small @big) 2)))))
 
-(defn smaller []
+(defn smaller
+  "The guess was too high, so lower it."
+  []
   (dosync
-   (ref-set *big* (dec (guess-my-number)))))
+   (ref-set big (dec (guess-my-number)))))
 
-(defn bigger []
+(defn bigger
+  "The guess was too low, so raise it."
+  []
   (dosync
-   (ref-set *small* (inc (guess-my-number)))))
+   (ref-set small (inc (guess-my-number)))))
 
-(defn start-over []
+(defn start-over
+  "Reset everything and prepare to start over."
+  []
   (dosync
-   (ref-set *small* 1)
-   (ref-set *big* 100)))
+   (ref-set small 1)
+   (ref-set big 100)))
 
