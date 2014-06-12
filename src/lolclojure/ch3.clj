bdf85ace32c29de71342d173b2a26fb8b1b65049
Joey Gibson <jgibson@lancope.com>
2014-06-12 08:49:34 -0400

    finished with chapter 3

--- src/lolclojure/ch3.clj
+++ src/lolclojure/ch3.clj
@@ -1,9 +1,23 @@
 (ns lolclojure.ch3)
 
+;; CL uses (defun...) to define functions, Clojure uses (defn...)
+(defn a-func
+  "Optional doc comment"
+  [x n]
+  (* x n))
+
 ;; CL uses (eq...) for basic equality; Clojure uses (=...)
 (= 'foo 'foo)
 
-;; CL has (expt) for exponents; Clojure has... nothing...
+;; CL has (expt) for exponents; Clojure has... nothing built in...
+;; You could use Java's Math.pow, but it only does doubles
+(Math/pow 2 2)
+(Math/pow 2 3)
+(Math/pow 2 10)
+(Math/pow 53N 53)
+
+;; Here are two expt functions, both of which are tail recursive
+;; The first uses a nested function
 (defn expt
   "Raise x to the nth power"
   [x n]
@@ -16,8 +30,9 @@
 (expt 2 2)
 (expt 2 3)
 (expt 2 10)
+(expt 53N 53)
 
-;; using loop instead of a nested function
+;; This one recurs on a loop instead of a nested function
 (defn expt
   "Raise x to the nth power"
   [x n]
@@ -31,3 +46,100 @@
 (expt 2 3)
 (expt 2 10)
 (expt 53N 53)
+
+;; CL uses (princ) (prin1) (print), etc. for output
+;; Clojure has (print) (println), etc.
+(print "He yelled \"Stop that thief!\" from the busy street.") ; no newline
+(println "He yelled \"Stop that thief!\" from the busy street.") ; newline
+
+;; In CL, the building block of a list is the cons cell. Clojure does
+;; not have cons cells. In most/all cases, a vector will suffice.
+(def pair [:a :b])                                          ; a vector of two items
+(def tuple [:a :b :c])                                      ; a vector of three items
+
+(vector 'chicken 'cat)                                      ; ~= (CHICKEN . CAT)
+
+;; The other example on page 39 don't have direct equivalents
+
+;; Clojure has (cons), but it adds an item to a list. It does not
+;; create connected cons cells.
+(cons 'beef (cons 'chicken '()))                            ; (beef chicken)
+(cons 'pork (cons 'beef (cons 'chicken ())))                ; (pork beef chicken)
+
+;; CL uses car, cdr, cadr, caddr, etc. Clojure has first, rest, second.
+(first '(pork beef chicken))                                ; (car '(pork beef chicken)) => pork
+(rest '(pork beef chicken))                                 ; (cdr '(pork beef chicken)) => (beef chicken)
+(second '(pork beef chicken))                               ; (cadr '(pork beef chicken)) => beef
+
+;; CL defines c*r functions, up to four levels deep. You *could* simulate these
+;; in Clojure like this
+(defn cdar
+  [lst]
+  (rest (first lst)))
+
+(cdar '((peas carrots tomatoes) (pork beef chicken)))       ; (carrots tomatoes)
+
+(defn cddr
+  [lst]
+  (rest (rest lst)))
+
+(cddr '((peas carrots tomatoes) (pork beef chicken) duck))  ; (duck)
+
+(defn caddr
+  [lst]
+  (first (rest (rest lst))))
+
+(caddr '((peas carrots tomatoes) (pork beef chicken) duck)) ; duck
+
+(defn cddar
+  [lst]
+  (rest (rest (first lst))))
+
+(cddar '((peas carrots tomatoes) (pork beef chicken) duck)) ; (tomatoes)
+
+(defn cadadr
+  [lst]
+  (first (rest (first (rest lst)))))
+
+(cadadr '((peas carrots tomatoes) (pork beef chicken) duck)) ; beef
+
+;; But destructuring is probably a better choice
+(let [[fst] '((peas carrots tomatoes) (pork beef chicken))]
+  (rest fst))                                               ; (carrots tomatoes) ; instead of cdar
+
+(let [[_ & rst] '((peas carrots tomatoes) (pork beef chicken) duck)]
+  (rest rst))                                               ; (duck) ; instead of cddr
+
+(let [[_ & rst] '((peas carrots tomatoes) (pork beef chicken) duck)
+      tail (rest rst)]
+  (first tail))                                             ; duck ; instead of caddr
+
+(let [[fst] '((peas carrots tomatoes) (pork beef chicken) duck)
+      tail (rest fst)]
+  (rest tail))                                              ; (tomatoes) ; instead of cddar
+
+(let [[_ & rst] '((peas carrots tomatoes) (pork beef chicken) duck)
+      fst (first rst)
+      tail (rest fst)]
+  (first tail))                                             ; beef ; instead of cadadr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
